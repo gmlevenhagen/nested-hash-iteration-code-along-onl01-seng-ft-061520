f0dcc6bee96b8323106f7db04d56ddebe0e3c5a5
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 12:54:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -27,14 +27,18 @@ contacts ={
        favorite_ice_cream_flavors: [ "strawberry", "cookie dough", "mint chip"]
     }
   }
+end
+
+
+
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
-
-
 end
