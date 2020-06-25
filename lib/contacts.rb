054d1769acfc2726278118bd6d46d1beb80b08b0
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 12:42:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ contact ={
   "Freddy Mercury" => {
        name: "Freddy",
        email: "freddy@mercury.com",
-       favorite_ice_cream_flavors: [cookie dough", "mint chip"]
+       favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
     }
   }
 end
